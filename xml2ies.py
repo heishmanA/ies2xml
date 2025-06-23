9ca89c7a72a88c7bb934ce4a59f6258f0f48a1ca
Aaron <aaronheishman5@outlook.com>
2025-06-23 18:10:51 -0400

    Added initial code for xml2ies.py with functionality to open a single file or batch open multiple files

---
+++ xml2ies.py
@@ -0,0 +1,83 @@
+import argparse
+import os
+from pathlib import Path
+import glob
+
+parser = argparse.ArgumentParser(
+    description = 'An .xml to .ies converter'
+)
+
+subparser = parser.add_subparsers(
+    help = 'subcommand help',
+    required = True,
+    dest = 'subcommand'
+)
+
+parser_file = subparser.add_parser(
+    'file',
+    help = 'file help'
+)
+
+parser_file.add_argument(
+    '--output', '-o',
+    required = False,
+    help = 'Optional output for a single file; overwrites default file',
+    type = Path
+)
+
+parser_file.add_argument(
+    'xml_file',
+    help = 'The xml file to convert',
+    type = Path
+)
+
+parser_batch = subparser.add_parser(
+    'batch',
+    help = 'batch help'
+)
+
+parser_batch.add_argument(
+    'directory',
+    help = 'The directory containing all .xml files to be batch converted',
+    type = Path
+)
+
+def verify_is_dir(dir: Path) -> bool:
+    """Simple function to verify if a path is a directory or not
+
+    Args:
+        dir (Path): The directory to verify
+
+    Returns:
+        boolean: True if the directory is a valid path or not, false otherwise
+    """
+    return os.path.isdir(dir)
+
+def convert_to_ies(file: Path, dest=None):
+    print(f'Converting {file.name} to .ies')
+    pass
+
+def batch_convert_to_ies(directory: Path):
+    """Converts all files within the given directory to convert all .ies files into .xml files
+
+    Args:
+        directory (Path): The directory containing the .xml files
+    """
+    if not verify_is_dir(directory):
+        print(f'Directory not found {directory}. Please verify the correct directory was given')
+        return
+    
+    for xml_file in directory.glob('*.xml'):
+        try:
+            convert_to_ies(xml_file)
+        except Exception as e:
+            print(f"""Exception caught: {e}' Skipping {xml_file}""")
+
+    
+if __name__ == "__main__":
+    args = parser.parse_args()
+    print(f'The subcommand chosen: {args.subcommand}')
+    if args.subcommand == 'file':
+        convert_to_ies(args.xml_file, args.output)
+    else:
+        batch_convert_to_ies(args.directory)
